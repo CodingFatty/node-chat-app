f47edc261a1467a42c27baa9180d65f4aae4e970
admin <admin@admins-iMac.local>
2018-03-01 14:35:55 +0800

    Scroll to bottom feature

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 var socket = io();
 
+function scrollToBottom() {
+  var messages = jQuery('#messages')
+  var newMessage = messages.children('li:last-child')
+
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+
+
+
+}
+
 socket.on('connect', function () {
   console.log('Connected to the server');
 
@@ -19,7 +38,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html)
-
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
   //
@@ -38,6 +57,7 @@ socket.on('newLocationMessage', function (message) {
   })
 
   jQuery('#messages').append(html)
+  scrollToBottom();
   // li.text(`${message.from} ${formattedTime}: `);
   // a.attr('href', message.url);
   // li.append(a);
